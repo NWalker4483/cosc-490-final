ed22169c9b74180197a4a35f8d141b308374e670
Nile Walker <niwal7@morgan.edu>
2021-02-21 22:47:32 -0500

    Added workflow files

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {BreedVoterComponent} from './breed-voter/breed-voter.component'
+const routes: Routes = [
+  {
+  path: 'vote',
+  component: BreedVoterComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
