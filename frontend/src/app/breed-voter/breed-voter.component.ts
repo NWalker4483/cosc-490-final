abc93cd367599e1421d73a09cb2e6febe67b7df6
Nile Walker <niwal7@morgan.edu>
2021-02-22 00:09:33 -0500

    Updated Index Page

--- frontend/src/app/breed-voter/breed-voter.component.ts
+++ frontend/src/app/breed-voter/breed-voter.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from '../api.service';
+import { Breed } from './breed.model';
 @Component({
   selector: 'app-breed-voter',
   templateUrl: './breed-voter.component.html',
   styleUrls: ['./breed-voter.component.css']
 })
 export class BreedVoterComponent implements OnInit {
-
-  constructor() { }
+  breedOptions: Breed[];
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
+    this.getBreeds();
   }
+
+  getBreeds(): void {
+    this.api.getBreeds().subscribe(data => this.breedOptions = data);
+  }
+
 
 }
