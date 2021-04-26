32dad79a393a58a681fbf8c93cacff4b2d5c40f7
NWalker4483 <niwal7@morgan.edu>
2021-04-26 15:16:49 -0400

    config

--- web-app/client/src/components/CastBallot.vue
+++ web-app/client/src/components/CastBallot.vue
@@ -1,46 +1,49 @@
 <template>
   <div class="posts">
     <h1>Cast Ballot</h1>
-    <input type="radio" id="one" value="Republican" v-model="picked">
-    <label for="one">Donald Trump (Republican)</label>
-    <br>
-    <input type="radio" id="two" value="Democrat" v-model="picked">
-    <label for="two">TBA (Democratic)</label>
-    <br>
-    <input type="radio" id="two" value="Green" v-model="picked">
-    <label for="two">TBA (Green Party)</label>
-    <br>
-    <input type="radio" id="two" value="Independent" v-model="picked">
-    <label for="two">TBA (Independent)</label>
-    <br>
-    <input type="radio" id="two" value="Libertarian" v-model="picked">
-    <label for="two">Increase Library Hours.</label>
-    <br>
-    <br>
- 
- <span v-if= " picked">
-  Picked:
-  <b> {{ picked }}</b>
-  </span?
-
-    <br>
-    <br>
+    <v-container class="my-5">
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg6 v-for="prop in propoals" :key="prop.alt">
+          <v-card raised class="text-xs-center ma-3">
+            <v-responsive class="pt-4">image</v-responsive>
+            <v-card-text>
+              <div class="subheading">{{ prop.alt }}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn flat color="grey">
+                <v-icon small left>home
+                  </v-icon>
+                  </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <span v-if="picked">
+      Picked:
+      <b> {{ picked }}</b>
+    </span>
+    <br />
+    <br />
     <!--span><b>{{ response }}</b></span><br /-->
     <form v-on:submit="castBallot">
       <!-- <input type="text" value="2sww593dc034wb2twdk91r" v-model="input.electionId"  >
       <br>-->
-      <input type="text" v-model="input.voterId" placeholder="Enter VoterId">
-      <br>
-      <input type="submit" value="Cast Vote">
-      <br>
+      <input type="text" v-model="input.voterId" placeholder="Enter VoterId" />
+      <br />
+      <input type="submit" value="Cast Vote" />
+      <br />
     </form>
 
-    <br>
+    <br />
     <span v-if="response">
       <b>{{ response }}</b>
     </span>
-    <br>
-    <vue-instant-loading-spinner id="loader" ref="Spinner"></vue-instant-loading-spinner>
+    <br />
+    <vue-instant-loading-spinner
+      id="loader"
+      ref="Spinner"
+    ></vue-instant-loading-spinner>
   </div>
 </template>
 
@@ -54,17 +57,39 @@ export default {
     return {
       input: {},
       picked: null,
-      response: null
+      response: null,
+      propoals: [
+        { alt: "Republican", 
+        image_url: "",
+        description: "Cats are dope sometimes",
+        title: ""},
+        { alt: "Democrat", 
+        image_url: "",
+        description: "Cats are dope sometimes",
+        title: ""},
+        { alt: "Green", 
+        image_url: "",
+        description: "Cats are dope sometimes",
+        title: ""},
+        { alt: "Independent", 
+        image_url: "",
+        description: "Cats are dope sometimes",
+        title: ""},
+        { alt: "Libertarian", 
+        image_url: "",
+        description: "Cats are dope sometimes",
+        title: ""},
+      ],
     };
   },
   components: {
-    VueInstantLoadingSpinner
+    VueInstantLoadingSpinner,
   },
   methods: {
     async castBallot() {
       await this.runSpinner();
 
-      const electionRes = await PostsService.queryWithQueryString('election');
+      const electionRes = await PostsService.queryWithQueryString("election");
 
       let electionId = electionRes.data[0].Key;
 
@@ -74,43 +99,37 @@ export default {
       console.log(this.input.voterId);
       this.response = null;
 
- 
-
       //error checking for making sure to vote for a valid party
-      if (this.picked === null ) {
-        console.log('this.picked === null')
+      if (this.picked === null) {
+        console.log("this.picked === null");
 
         let response = "You have to pick a party to vote for!";
         this.response = response;
         await this.hideSpinner();
-      
       } else if (this.input.voterId === undefined) {
-        console.log('this.voterId === undefined')
+        console.log("this.voterId === undefined");
 
         let response = "You have to enter your voterId to cast a vote!";
         this.response = response;
         await this.hideSpinner();
-
       } else {
-
         const apiResponse = await PostsService.castBallot(
           electionId,
           this.input.voterId,
           this.picked
         );
 
-        console.log('apiResponse: &&&&&&&&&&&&&&&&&&&&&&&');
+        console.log("apiResponse: &&&&&&&&&&&&&&&&&&&&&&&");
         console.log(apiResponse);
 
         if (apiResponse.data.error) {
-          this.response= apiResponse.data.error;
+          this.response = apiResponse.data.error;
           await this.hideSpinner();
         } else if (apiResponse.data.message) {
-          this.response= `Could not find voter with voterId ${this.input.voterId}
+          this.response = `Could not find voter with voterId ${this.input.voterId}
             in the state. Make sure you are entering a valid voterId`;
           await this.hideSpinner();
-        } 
-        else {
+        } else {
           let response = `Successfully recorded vote for ${this.picked} party 
             for voter with voterId ${apiResponse.data.voterId}. Thanks for 
             doing your part and voting!`;
@@ -128,7 +147,7 @@ export default {
     },
     async hideSpinner() {
       this.$refs.Spinner.hide();
-    }
-  }
+    },
+  },
 };
 </script>
