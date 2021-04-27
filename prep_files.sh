324ab471fac42d99ba74d3ec3129ab52fa1d2879
NWalker4483 <niwal7@morgan.edu>
2021-04-27 16:58:10 -0400

    prep fixes

--- prep_files.sh
+++ prep_files.sh
@@ -1,3 +1,3 @@
-# rm -rf frontend # Remove front end code to save space
+rm -rf contract # Remove front end code to save space
 # mv server/* . # Move server files to root so that glitch can find them
 # rm -rf server # remove leftover folder
