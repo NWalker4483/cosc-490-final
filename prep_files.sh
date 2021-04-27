b53a77fb55a1f1e617c5c4a830e497a615dbcdda
NWalker4483 <niwal7@morgan.edu>
2021-04-27 18:39:53 -0400

    push

--- prep_files.sh
+++ prep_files.sh
@@ -2,7 +2,7 @@ rm -rf contract
 mv web-app/* .
 rm -rf server/static/ 
 mkdir -p server/static/ 
-cp client/dist/* server/static/
+cp -r client/dist/* server/static/
 rm -rf web-app client
 mv server/* .
 rm .gitignore
